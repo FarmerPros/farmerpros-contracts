dc0d973426b74d9b9bbd1b0e72d224dfca82be31
FarmerPros <farmer@farmerpros.farm>
2022-04-20 09:13:20 -0700

    Added token exhange and reentrancy guard

--- FarmToken.sol
+++ FarmToken.sol
@@ -4,10 +4,32 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-abstract contract FarmToken is ERC20, Ownable {
+abstract contract FarmToken is ERC20, Ownable, ReentrancyGuard {
     event Mint(uint256 amount);
     event Burn(uint256 amount);
+
+    FarmToken public presaleToken;
+    uint256 public presaleExchangeDeadline;
+    address public firstOwner;
+
+    modifier beforeDeadline() {
+        require(
+            presaleExchangeDeadline == 0 ||
+                block.timestamp < presaleExchangeDeadline,
+            "Deadline to exchange presale token has passed."
+        );
+        _;
+    }
+
+    modifier onlyOwnerAndFirstOwner() {
+        require(
+            owner() == msg.sender || owner() == firstOwner,
+            "You don't have the ownership."
+        );
+        _;
+    }
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
@@ -19,19 +41,35 @@ abstract contract FarmToken is ERC20, Ownable {
         emit Burn(amount);
     }
 
-    function transferToken(address recipient, uint256 amount)
+    /**
+    Exchanges the presale token until the specified deadline
+     */
+    function exchangePresaleToken(uint256 exchangeAmount)
         external
-        onlyOwner
-        returns (bool)
+        nonReentrant
+        beforeDeadline
     {
-        return transfer(recipient, amount);
+        ERC20(presaleToken).transferFrom(
+            msg.sender,
+            address(this),
+            exchangeAmount
+        );
+        _transfer(owner(), msg.sender, exchangeAmount);
     }
 
-    function transferTokenFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external onlyOwner returns (bool) {
-        return transferFrom(sender, recipient, amount);
+    function setPresaleToken(FarmToken token) external onlyOwner {
+        presaleToken = token;
+    }
+
+    function setPresaleExchangeDeadline(uint256 deadline)
+        external
+        onlyOwnerAndFirstOwner
+    {
+        presaleExchangeDeadline = deadline;
+    }
+
+    function transferOwnershipTo(address newOwner) external onlyOwner {
+        firstOwner = newOwner;
+        transferOwnership(newOwner);
     }
 }
